ceab37026a82104de546305fe3d459a65dafbf43
jesustissera <jesustissera@gmail.com>
2025-08-07 02:55:28 -0300

    cargo otros campos con datos de seleccion

--- src/plugins/my-custom-fields/admin/src/components/SelectCustomize/index.tsx
+++ src/plugins/my-custom-fields/admin/src/components/SelectCustomize/index.tsx
@@ -3,14 +3,18 @@ import React, { useState, useEffect } from 'react';
 const SelectCustomize = (props: any, ref: any) => {
   const { attribute, disabled, intlLabel, name, onChange, required, value } = props;
   const [productos, setProductos] = useState<any[]>([]);
-  
+
   const queryParams = new URLSearchParams(window.location.search);
   const localId = queryParams.get('localId');
-    
+
+  const nameSplit = name.split('.');
+  const index = parseInt(nameSplit[1]);
+
   useEffect(() => {
     fetch(`/api/productos?populate=*&filters[locales][id][$eq]=${localId}`)
       .then((res) => res.json())
       .then((data) => {
+        console.log(data);
         if (!data?.data) return;
         setProductos(data.data);
       })
@@ -20,14 +24,39 @@ const SelectCustomize = (props: any, ref: any) => {
   }, []);
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedId = e.target.value;
+    const selectedProducto = productos.find((p) => p.id === parseInt(selectedId));
+
     onChange({
-      target: { name, type: attribute.type, value: e.target.value },
+      target: { name, type: attribute.type, value: selectedId },
     });
+    console.log(selectedProducto);
+
+    if (selectedProducto) {
+      const precio = selectedProducto.precio;
+      const stock = selectedProducto.stock;
+
+      onChange({
+        target: {
+          name: `Productos.${index}.cantidad`,
+          type: 'number',
+          value: stock > 0 ? 1 : 0,
+        },
+      });
+
+      onChange({
+        target: {
+          name: `Productos.${index}.total`,
+          type: 'number',
+          value: precio,
+        },
+      });
+    }
   };
 
   return (
     <>
-      <label htmlFor={name}>{intlLabel?.defaultMessage || name}</label>
+      <label htmlFor={name}>Producto</label>
       <select
         name={name}
         disabled={disabled}
