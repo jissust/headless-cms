422bb78ba78f23a28212da0ae4b5db8dfb77eb20
jesustissera <jesustissera@gmail.com>
2025-08-15 04:48:35 -0300

    prueba para url que se genera con error

--- config/plugins.ts
+++ config/plugins.ts
@@ -5,21 +5,5 @@ export default () => ({
   },
   "strapi-plugin-pdf-creator": {
     enabled: true,
-    config: {
-      templates: [
-        {
-          uid: "api::remito.remito", // UID de tu colección
-          path: "public/remito_template.pdf", // Ruta al PDF base
-          fields: {
-            nombre: "nombre",
-            apellido: "apellido",
-            telefono: "telefono",
-            fecha: "fecha",
-            descripcion: "descripcion",
-            precio: "precio",
-          },
-        },
-      ],
-    },
   },
 });
