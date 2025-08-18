bbeca5fb45788a57a9fc656b1db4e7048245dbdf
jesustissera <jesustissera@gmail.com>
2025-08-18 17:51:01 -0300

    fix 5: error en descarga pdf

--- src/middlewares/pdf-fix.ts
+++ src/middlewares/pdf-fix.ts
@@ -10,41 +10,45 @@ export default (config: any, { strapi }: { strapi: typeof global.strapi }) => {
           documentId: reqData.templateId,
           populate: ["file"],
         });
-        strapi.log.info(template2)
-        console.log(template2)
+
       if (template2.file.url.startsWith("http")) {
-        //const hash = template2.file.hash;
-        //const ext = template2.file.ext;
-        //const urlFinal = `public/uploads/${hash}${ext}`;
-
-        console.log("http");
-
-        const pdfService = strapi
-          .plugin("strapi-plugin-pdf-creator")
-          .controller("pdfGenerator");
-
-        const response = await fetch(`${template2.file.url}`);
-        if (!response.ok) {
-          throw new Error(
-            `No se pudo descargar el PDF: ${response.statusText}`
-          );
-        }
+        strapi.log.info("EN PRODUCCIÓN")
+        const response = await fetch(
+          `https://sublime-happiness-a3285d84ce.media.strapiapp.com/remito_f6ae77075e.pdf`
+        );
         const templateBytes = Buffer.from(await response.arrayBuffer());
-        console.log("TEMPLATEBYTES");
-        const originalCreate = pdfService.create;
-        pdfService.create = async function (ctx: any) {
-          return await strapi
-            .plugin("strapi-plugin-pdf-creator")
-            .service("service")
-            .createPDF(
-              templateBytes,
-              reqData,
-              template2.name,
-              template2.flattenDocument,
-              true
-            );
-        };
-        //ctx.isTest = true;
+
+        let docData = await strapi.documents(reqData.collectionType).findFirst({
+          filters: { documentId: reqData.documentId },
+          populate: "*",
+        });
+        docData = await strapi
+          .plugin("strapi-plugin-pdf-creator")
+          .service("images")
+          .BufferIamgesOnData(docData, false);
+        console.log(docData);
+
+        const conf = strapi.config.get(`plugin::strapi-plugin-pdf-creator`);
+        console.log(conf["beautifyDate"]);
+        const genDoc = await strapi
+          .plugin("strapi-plugin-pdf-creator")
+          .service("service")
+          .createPDF(
+            templateBytes,
+            docData,
+            template2.name,
+            template2.flattenDocument,
+            conf["beautifyDate"]
+          );
+
+        ctx.res.writeHead(200, {
+          "Content-Length": Buffer.byteLength(genDoc),
+          "Content-Type": "application/pdf",
+          "Content-disposition": `attachment; filename=remito.pdf`,
+        });
+        ctx.res.end(genDoc);
+
+        return;
       }
     }
 
