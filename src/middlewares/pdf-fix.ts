c408a8078b2735cd8ce2913990a7f58960037d8f
jesustissera <jesustissera@gmail.com>
2025-08-18 15:25:46 -0300

    fix: error en descarga pdf

--- src/middlewares/pdf-fix.ts
+++ src/middlewares/pdf-fix.ts
@@ -1,19 +1,44 @@
+import fs from "fs";
 export default (config: any, { strapi }: { strapi: typeof global.strapi }) => {
   return async (ctx: any, next: () => Promise<any>) => {
     if (ctx.url.startsWith("/strapi-plugin-pdf-creator/create-pdf")) {
-      strapi.log.info("🔄 Interceptando request de PDF Creator");
-
-      //ctx.request.body.template.file.url = "/remito_f6ae77075e.pdf";
       const reqData = ctx.request.body?.data;
+
       const template2 = await strapi
         .documents(`plugin::strapi-plugin-pdf-creator.template`)
         .findOne({
           documentId: reqData.templateId,
           populate: ["file"],
         });
-      console.log(template2);
-      ctx.isTest = true;
-      strapi.log.info(`📂 Nueva URL de template: ${ctx.request}`);
+
+      if (template2.file.url.startsWith("http")) {
+        const hash = template2.file.hash;
+        const ext = template2.file.ext;
+        const urlFinal = `${hash}${ext}`;
+        
+        console.log("http");
+        console.log(urlFinal);
+        
+        const pdfService = strapi
+          .plugin("strapi-plugin-pdf-creator")
+          .controller("pdfGenerator");
+
+        const templateBytes = fs.readFileSync(`${urlFinal}`);
+        const originalCreate = pdfService.create;
+        pdfService.create = async function (ctx: any) {
+          return await strapi
+            .plugin("strapi-plugin-pdf-creator")
+            .service("service")
+            .createPDF(
+              templateBytes,
+              reqData,
+              template2.name,
+              template2.flattenDocument,
+              true
+            );
+        };
+        ctx.isTest = true;
+      }
     }
 
     await next();
