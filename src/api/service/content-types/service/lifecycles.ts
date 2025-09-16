970ec75dc47e24c5743d39475e884ab78ad3e076
jesustissera <jesustissera@gmail.com>
2025-09-16 11:14:06 -0300

    N° de orden se genera de manera automática

---
+++ src/api/service/content-types/service/lifecycles.ts
@@ -0,0 +1,21 @@
+import { errors } from "@strapi/utils";
+
+export default {
+  async afterCreate(event) {
+    const { result } = event;
+    console.log("Service creado:", result.id);
+    if (!result.numero_de_orden) {
+      await strapi.db.query("api::service.service").update({
+        where: { id: result.id },
+        data: { numero_de_orden: result.id },
+      });
+      console.log(`✅ numero_de_orden asignado al crear: ${result.id}`);
+    }
+  },
+  async beforeUpdate(event) {
+    const { data, where } = event.params;
+    if (!data.numero_de_orden) {
+      event.params.data.numero_de_orden = where.id;
+    }
+  },
+};
